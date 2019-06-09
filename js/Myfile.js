8458fb1482ab6cb073b323b683892cd1d696d1b1
Konstantine899 <46957212+Konstantine899@users.noreply.github.com>
2019-06-09 14:05:40 +0300

    001

---
+++ js/Myfile.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+$('#nav li').slideUp(0.1).slideDown(1000);
+
+    $('#peopleImg').animate({'width' : '1px'},0.1);
+    $('#peopleImg').animate({'width' : '388px'},1000);
+    
+    $('').slideUp(0.1).slideDown(1000);
+     $('#slogan').slideUp(0.1).slideDown(1000);
+    $('#colors').slideUp(0.1).slideDown(2000);
+    
+      
+     
+});
+
+
